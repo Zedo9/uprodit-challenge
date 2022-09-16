155e3b15555db82ad346bd22afb3cc9d7ebe0b66
Chedly Zouche <chedlyzouche9999@outlook.com>
2022-09-16 13:39:43 +0100

    Add vuetify

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,12 @@
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+// Vuetify
+import { createVuetify } from "vuetify";
+
+export default createVuetify({ components, directives });
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
